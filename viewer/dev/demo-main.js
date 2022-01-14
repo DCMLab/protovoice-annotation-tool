6f94cdc318ac9735c0947e3064fff3356294f126
Christoph Finkensiep <chfin@chfin.de>
2022-01-14 16:01:55 +0100

    fix more viewer imports

--- viewer/dev/demo-main.js
+++ viewer/dev/demo-main.js
@@ -1,7 +1,10 @@
-const viewer = require("../../output/Main/index.js");
+const viewer = require("../output/Main/index.js");
 
 function selectFile() {
     const file = this.files[0];
+    // clear target div
+    document.getElement.ById("widget").innerHTML = "";
+    // load file and create viewer widget
     file.text().then(json => viewer.createViewer("#widget", json));
 }
 
