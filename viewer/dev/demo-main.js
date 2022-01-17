7c5fc50bd74fda7f712de32ab5a17618bfe113e2
Christoph Finkensiep <chfin@chfin.de>
2022-01-17 10:19:00 +0100

    fix typo

--- viewer/dev/demo-main.js
+++ viewer/dev/demo-main.js
@@ -3,7 +3,7 @@ const viewer = require("../output/Main/index.js");
 function selectFile() {
     const file = this.files[0];
     // clear target div
-    document.getElement.ById("widget").innerHTML = "";
+    document.getElementById("widget").innerHTML = "";
     // load file and create viewer widget
     file.text().then(json => viewer.createViewer("#widget", json));
 }
